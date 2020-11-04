da62a3e0f0657aa7e4e62df4a28a41705c6bd397
Ginel <186165@iessierradeguara.com>
2020-11-04 17:16:18 +0100

    Java script
    Con valores numericos y texto
    
    Con programnacion (if)

---
+++ web-js1/app.js
@@ -0,0 +1,111 @@
+alert("Hola mundo");
+
+//Comenmtario de una linea
+
+/* Esto es un comentario
+multilinea*/
+
+//Tipos de datos
+
+//Tipo cadena de texto o string
+
+// Cntrl + Shift + 7 para comentar lo seleccionado (empiezan por alert)
+// Cntrl + Shift + 7 para comentar lo seleccionado
+// Cntrl + Shift + 7 para comentar lo seleccionado
+
+
+// alert("Esto es una cadena de texto");
+// alert('Esto tambien es una cadena de texto');
+// alert("I'm the \"teacher\"");
+// alert('I\'m the teacher');
+
+
+
+// Tipo número entero
+// Se pueden ussar operadores aritméticos
+// alert(2+3);
+// alert(5-2);
+// alert(2*2);
+// alert(4/2);
+
+
+
+// Tipo número decimal
+// Se pueden ussar operadores aritméticos
+// alert(2.5+3);
+// alert(5.7-2);
+// alert(2.4*2);
+// alert(4.5/2);
+
+
+
+// Tipo boolano
+// operaciones lógicas && ||
+
+// Y
+// alert(true && true );
+// O
+// alert(false || false);
+
+
+
+let mensaje = "Hola MUNDO";
+let nombre = "Ginel";
+// alert(mensaje);
+// alert("Hola "+nombre);
+// console.log(mensaje);
+
+
+// let numero = 3;
+// numero = 3 + 2;
+// numero++;
+// alert(numero);
+// numero--;
+// alert(numero);
+
+
+// let v1 = 3;
+// let v2 = 4;
+// let v3 = 6;
+// alert(v1 + v2 - v3);
+
+
+/*declarar 2 variables con valores numericos
+
+declarar 2 variables llamada resultado que contenga el 
+producto de las variables anteriores
+
+imprimir en un alert el resultado*/
+
+// let va1 = 10;
+// let va2 = 4;
+// let resultado = (va1 - va2);
+// alert(resultado);
+
+
+
+// document.write("<h1>Hola Mundo</h1>");
+
+// alert("Mira el h1");
+// let elemento = document.getElementById("encabezado");
+// elemento.innerHTML = "Hola clase";
+// alert("Mira el h1");
+// elemento.innerHTML = "Hola clase1";
+// alert("Mira el h1");
+// elemento.innerHTML = "Hola clase2";
+// alert("Mira el h1");
+// elemento.innerHTML = "Hola clase3";
+
+
+
+let resultado
+
+resultado = confirm("¿Quieres que te salude?");
+
+if (resultado == true) {
+    alert("Hola");
+}
+alert("fin del programa");
+
+console.log(resultado);
+
